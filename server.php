f08ff5745bcef27874612bf9e807da0ad008cf4c
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-03 16:03:46 +0200

    Stampata lunghezza del paragrafo e strtoupper (uppercase).

--- server.php
+++ server.php
@@ -2,6 +2,12 @@
 
 $paragrafo = $_POST["paragrafo"];
 
-echo "Il tuo motto è..." . $paragrafo
+echo "Il tuo motto è..." . $paragrafo;
+// Stampa a schermo il paragrafo inserito dall'utente.
 
+echo strlen ($paragrafo);
+// Stampare a schermo la sua lunghezza del paragrafo inserito dall'utente.
+
+echo strtoupper ($paragrafo);
+// Converte il paragrafo inserito dall'utente in uppercase.
 ?>
