a83398ebfa4653c55ee5d7ddd8017982bb156225
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-03 16:40:23 +0200

    Sostituita parola con tre asterischi. Inserito br tra un echo e l'altro.

--- server.php
+++ server.php
@@ -1,13 +1,16 @@
 <?php
 
 $paragrafo = $_POST["paragrafo"];
+$censura = $_POST["censura"];
 
-echo "Il tuo motto è..." . $paragrafo;
+echo "Il tuo motto è..." . $paragrafo . "<br>";
 // Stampa a schermo il paragrafo inserito dall'utente.
 
-echo strlen ($paragrafo);
-// Stampare a schermo la sua lunghezza del paragrafo inserito dall'utente.
+echo "La lunghezza del paragrafo è di caratteri: " . strlen ($paragrafo) . "<br>";
+// Stampare a schermo la lunghezza del paragrafo inserito dall'utente.
 
-echo strtoupper ($paragrafo);
+echo "Il paragrafo in uppercase è: " . strtoupper ($paragrafo) . "<br>";
 // Converte il paragrafo inserito dall'utente in uppercase.
+
+echo "Ho censurato la tua parola con: " . str_replace($censura, "***", $censura);
 ?>
