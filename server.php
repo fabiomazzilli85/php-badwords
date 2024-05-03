faca526c1cb8202547df4cc4b9efdb07aed99c29
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-03 18:17:02 +0200

    Create $paragrafo_modificato e $lunghezza_paragrafo_modificato. Lavorato con strlen e e replace. Commenti utili.

--- server.php
+++ server.php
@@ -2,16 +2,23 @@
 
 $paragrafo = $_POST["paragrafo"];
 $censura = $_POST["censura"];
+$paragrafo_modificato = str_replace($censura, "***", $paragrafo);
+// Nella riga precedente ho creato una variabile che accetta tre argomenti (censura, *** e paragrafo). 
+// Questa  variabile mi sarà utile successivamente. 
+// Prende un input da censura, lo trasforma in *** e lo va a sostituire all'interno del paragrafo.  
 
-echo "Il tuo motto è..." . $paragrafo . "<br>";
+
+echo "L'incipit di Anna Karenina è: " . $paragrafo . "<br>";
 // Stampa a schermo il paragrafo inserito dall'utente.
 
-echo "La lunghezza del paragrafo è di caratteri: " . strlen ($paragrafo) . "<br>";
+echo "La lunghezza dell`incipit del romanzo è di caratteri: " . strlen ($paragrafo) . "<br>";
 // Stampare a schermo la lunghezza del paragrafo inserito dall'utente.
 
-echo "Il paragrafo in uppercase è: " . strtoupper ($paragrafo) . "<br>";
-// Converte il paragrafo inserito dall'utente in uppercase.
+echo "Ho censurato la parola che hai cercato. Il nuovo testo: " . $paragrafo_modificato . "<br>";
+// Richiamo la variabile $paragrafo_modificato creata precedentemente.
 
-echo "Ho censurato la tua parola con: " . str_replace($censura, "***", $censura);
-// Ho convertito la parola inserita dall'utente con tre asterischi.
+$lunghezza_paragrafo_modificato = strlen(str_replace("***", "", $paragrafo_modificato));
+echo "La lunghezza del nuovo incipit è di caratteri: " . $lunghezza_paragrafo_modificato;
+// Ho creato la variabile $lunghezza_paragrafo_modificato.
+// Da $paragrafo_modificato ho sottrtratto i tre asterischi sostituendoli con un argomento vuoto ("") per poi contare la lunghezza (strlen) del nuovo testo.
 ?>
